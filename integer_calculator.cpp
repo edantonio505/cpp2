3753b0f05d7da980e2aa8df389419617a0eb39a6
edantonio505 <edantonio505@gmail.com>
2017-11-20 13:55:00 -0500

    m

---
+++ integer_calculator.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+int main()
+{
+	int firstnumber;
+	int secondnumber;
+
+	cout << "Give me a number: ";
+	cin >> firstnumber;
+
+	cout << "Give me a second number: ";
+	cin >> secondnumber;
+
+	cout << "The sum of the two numbers: " << firstnumber + secondnumber << endl;
+	cout << "The difference of the 2 numbers: " << firstnumber - secondnumber << endl;
+	cout << "The product of the 2 numbers: " << firstnumber * secondnumber << endl;
+	cout << "The quotient of the two numbers: " << firstnumber / secondnumber << endl;
+	cout << "with a remainder of: " << firstnumber % secondnumber << endl;
+	return 0;
+}
